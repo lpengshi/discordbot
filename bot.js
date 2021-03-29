e7d8fe97eb219098e8f92b3e470e208ae4202d6e
lpengshi <laips90@hotmail.com>
2021-03-29 23:05:01 +0800

    insert quotes

--- bot.js
+++ bot.js
@@ -3,15 +3,19 @@ require('dotenv').config();
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const quotes = require('./quotes');
+const list = quotes.getQuotes;
 client.login(process.env.DISCORD_TOKEN);
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    if (msg.content === 'hello') {
-      msg.reply('Whats up?');
-    }
-  });
+client.on('message', (msg) => {
+	if (msg.content === '/date') {
+		msg.reply(`Today Date: ${new Date()}`);
+	} else if (msg.content.substring(0, 1) == '/') {
+		let index = Math.floor(Math.random() * 1000);
+		msg.reply(`${list[index].author} once said: ${list[index].text}`);
+	}
+});
